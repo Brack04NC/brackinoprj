3259d35610286483287ffacb9b8f6a8d22590ea5
Brack04NC <ninapepe2004@gmail.com>
2024-03-16 00:21:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Animazione scroll su click sui link di navigazione
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
