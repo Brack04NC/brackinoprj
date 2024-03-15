97760a2fd34aefdccbfe7960b4d65baf24be670f
Brack04NC <ninapepe2004@gmail.com>
2024-03-15 23:30:51 +0100

    Modifiche per il funzionamento

--- save_message.php
+++ save_message.php
@@ -1,8 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
-    $file = fopen("chat_history.txt", "a");
-    fwrite($file, $message);
+    $file = fopen("chat_history.txt", "a"); // Apre il file in modalità append
+    fwrite($file, $message . PHP_EOL); // Aggiunge il messaggio al file con un ritorno a capo
     fclose($file);
 }
 ?>
